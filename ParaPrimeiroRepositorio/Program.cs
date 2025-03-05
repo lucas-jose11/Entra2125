492484a080d8c7b673af7381069ddd6cd0d94373
Lucas José <lucasjosesilva012@gmail.com>
2025-03-05 10:52:41 -0300

    inicio

---
+++ ParaPrimeiroRepositorio/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ParaPrimeiroRepositorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
+//pra primeiro respoistório
